847d5e648465c83682b630e07bea09495bb39a14
Captainimal <jev.aln187@gmail.com>
2024-02-07 10:30:17 +0700

    Showing Mahasiswa data on Database

--- controller.js
+++ controller.js
@@ -4,6 +4,17 @@ var response = require('./res');
 var connection = require('./connection');
 
 let port = 3000;
-exports.index = function(req, res){
+exports.index = function (req, res) {
     response.ok("REST App is running on Port: " + port, res)
+};
+
+// Showing all Mahasiswa data
+exports.showingAllMahasiswa = function (req, res) {
+    connection.query('SELECT * FROM `mahasiswa`', function (error, rows, fields) {
+        if(error){
+            connection.log(error);
+        } else {
+            response.ok(rows, res);
+        }
+    });
 };
